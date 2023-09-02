78c6ebe2af2a37f1df594c10dee717ffc28cc032
nehapsar <syedaneha585@gmail.com>
2023-09-02 19:32:41 +0530

    concatination of the  column

---
+++ sql4.java
@@ -0,0 +1,38 @@
+select concat(fname,' ',lname) from student;
++-------------------------+
+| concat(fname,' ',lname) |
++-------------------------+
+| syeda neha              |
+| aiza mehreen            |
+| syed asger              |
+| k suma                  |
+| s apsar                 |
++-------------------------+
+5 rows in set (0.00 sec)
+
+mysql> select concat(fname,' ',lname) from student as name;
++-------------------------+
+| concat(fname,' ',lname) |
++-------------------------+
+| syeda neha              |
+| aiza mehreen            |
+| syed asger              |
+| k suma                  |
+| s apsar                 |
++-------------------------+
+5 rows in set (0.00 sec)
+
+mysql> select concat(fname,' ',lname) as name from student ;
++--------------+
+| name         |
++--------------+
+| syeda neha   |
+| aiza mehreen |
+| syed asger   |
+| k suma       |
+| s apsar      |
++--------------+
+5 rows in set (0.00 sec)
+
+mysql> 
+
